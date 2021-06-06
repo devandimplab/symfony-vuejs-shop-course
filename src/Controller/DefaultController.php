4e9b292bcfb08fe87813c5ff7d626f76a9e09246
Alexander Bruk <alexander@ranked-choice.com>
2021-06-06 16:14:13 +0300

    Created page 'Edit Product'

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,7 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Form\EditProductFormType;
+use phpDocumentor\Reflection\Types\Integer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,20 +28,30 @@ class DefaultController extends AbstractController
     }
 
     /**
-     * @Route("/product-add", methods="GET", name="product_add")
+     * @Route("/edit-product/{id}", methods="GET|POST", name="product_edit", requirements={"id"="\d+"})
+     * @Route("/add-product", methods="GET|POST", name="product_add")
      */
-    public function productAdd(Request $request): Response
+    public function editProduct(Request $request, int $id = null): Response
     {
-        $product = new Product();
-        $product->setTitle('Product '.rand(1, 100));
-        $product->setDescription('smth');
-        $product->setPrice(10);
-        $product->setQuantity(1);
-
         $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($product);
-        $entityManager->flush();
 
-        return $this->redirectToRoute('homepage');
+        if ($id) {
+            $product = $entityManager->getRepository(Product::class)->find($id);
+        } else {
+            $product = new Product();
+        }
+        $form = $this->createForm(EditProductFormType::class, $product);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('product_edit', ['id' => $product->getId()]);
+        }
+
+        return $this->render('main/default/edit_product.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ src/Form/EditProductFormType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class EditProductFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'Title (from class)',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([], 'Should be filled')
+                ]
+            ])
+            ->add('price', NumberType::class, [
+                'label' => 'Price (from class)',
+                'scale' => 2,
+                'html5' => true,
+                'attr' => [
+                    'step' => '0.01'
+                ]
+            ])
+            ->add('quantity')
+            ->add('description')
+            ->add('isPublished')
+            ->add('isDeleted')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
