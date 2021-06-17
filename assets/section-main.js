df58657b925d8e02cdc0d23583541d4410bfdfdf
Alexander Bruk <alexander@ranked-choice.com>
2021-06-17 18:07:33 +0300

    Configured webpack and compiled base scripts

---
+++ assets/section-main.js
@@ -0,0 +1,19 @@
+import './css/section/main/libs.scss';
+
+import './css/section/main/_alert.css';
+import './css/section/main/_banner.css';
+import './css/section/main/_base.css';
+import './css/section/main/_button.css';
+import './css/section/main/_cart.css';
+import './css/section/main/_cart_dropdown.css';
+import './css/section/main/_footer.css';
+import './css/section/main/_form.css';
+import './css/section/main/_header.css';
+import './css/section/main/_header_menu.css';
+import './css/section/main/_pagination.css';
+import './css/section/main/_product.css';
+import './css/section/main/_product_list.css';
+import './css/section/main/_spinner.css';
+import './css/section/main/_typography.css';
+
+require('./js/section/main/utils/menu')
