165158ae9590a6052ab7161c697cbd84f7f806c9
Alexander Bruk <alexander@ranked-choice.com>
2021-06-28 16:02:02 +0300

    Fixed config.yml

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -7,4 +7,4 @@ symfony console doctrine:database:create
 symfony console doctrine:schema:update --force
 symfony console hautelook:fixtures:load -n
 
-symfony php ./vendor/bin/phpunit --testdox --group unit,integration,functional
+symfony php ./vendor/bin/phpunit --testdox --group unit,integration,functional,functional-selenium
