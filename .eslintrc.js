d1a93a65456cf169a5feafe2cb4b8d9e01420687
Alexander Bruk <alexander@ranked-choice.com>
2021-06-28 14:49:10 +0300

    1. Added php-cs-fixer,phpstan,eslint;

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: [
+        "eslint:recommended",
+        "plugin:vue/recommended",
+        "plugin:prettier/recommended"
+    ],
+    env: {
+        node: true
+    }
+}
