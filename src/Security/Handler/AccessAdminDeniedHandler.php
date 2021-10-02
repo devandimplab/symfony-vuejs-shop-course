953bbbadff0b928091dad2d319c74e354ac23fbc
Alexander Bruk <alexander@ranked-choice.com>
2021-10-02 13:37:16 +0300

    Redirect user to login form if happend event Access Denied

---
+++ src/Security/Handler/AccessAdminDeniedHandler.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Security\Handler;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessAdminDeniedHandler implements AccessDeniedHandlerInterface
+{
+    private $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    public function handle(Request $request, AccessDeniedException $accessDeniedException): ?Response
+    {
+        // add a custom flash message and redirect to the login page
+        $request->getSession()->getFlashBag()->add('warning', 'You have to login in order to access this page.');
+
+        return new RedirectResponse($this->urlGenerator->generate('admin_security_login'));
+    }
+}
