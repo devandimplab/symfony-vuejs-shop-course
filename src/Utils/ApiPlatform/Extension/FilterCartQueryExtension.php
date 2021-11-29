0562a196d8ce8069d80457419bdbc1fd71af2ee4
Alexander Bruk <alexander@ranked-choice.com>
2021-11-29 20:13:57 +0300

    issue-23

--- src/Utils/ApiPlatform/Extension/FilterCartQueryExtension.php
+++ src/Utils/ApiPlatform/Extension/FilterCartQueryExtension.php
@@ -6,11 +6,12 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryCollectionExtensionInter
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Extension\QueryItemExtensionInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use App\Entity\Cart;
-use App\Entity\Product;
 use App\Entity\User;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class FilterCartQueryExtension implements QueryCollectionExtensionInterface, QueryItemExtensionInterface
 {
@@ -19,9 +20,15 @@ class FilterCartQueryExtension implements QueryCollectionExtensionInterface, Que
      */
     private $security;
 
-    public function __construct(Security $security)
+    /**
+     * @var Request
+     */
+    private $request;
+
+    public function __construct(Security $security, RequestStack $request)
     {
         $this->security = $security;
+        $this->request = $request;
     }
 
     public function applyToCollection(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
@@ -42,17 +49,39 @@ class FilterCartQueryExtension implements QueryCollectionExtensionInterface, Que
 
         $user = $this->security->getUser();
 
-        if ($user instanceof User && $user->isAdminRole()) {
+        /**
+         * This is just an example of a check.
+         * If your project doesn't need this check, just remove the method and this check.
+         */
+        if ($this->displayAllForAdmin($user)) {
             return;
         }
 
         $rootAlias = $queryBuilder->getRootAliases()[0];
 
         $request = Request::createFromGlobals();
-        $phpSessId = $request->cookies->get("PHPSESSID");
+        $cartToken = $request->cookies->get("CART_TOKEN");
 
         $queryBuilder->andWhere(
-            sprintf("%s.sessionId = '%s'", $rootAlias, $phpSessId)
+            sprintf("%s.token = '%s'", $rootAlias, $cartToken)
+        );
+    }
+
+    /**
+     * If you want to show all carts in the admin section (only for admin)
+     * Add query param "context = admin"
+     *
+     * Ex.: https://127.0.0.1:8000/api/carts?page=1&context=admin
+     *
+     * @param UserInterface $user
+     * @return bool
+     */
+    private function displayAllForAdmin(UserInterface $user): bool
+    {
+        return (
+            $user instanceof User
+            && $user->isAdminRole()
+            && $this->request->getCurrentRequest()->get('context') === 'admin'
         );
     }
 }
